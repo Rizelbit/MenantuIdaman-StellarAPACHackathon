6860d0b01518daaf8b0093dace6ff363ded70e48
andhikalucas <13523014@std.stei.itb.ac.id>
2026-07-13 00:19:22 +0700

    chore: build

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.kirimin.app"
+    namespace = "com.example.kirimin"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,7 +15,8 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.kirimin.app"
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.kirimin"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
